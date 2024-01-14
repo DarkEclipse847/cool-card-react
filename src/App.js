b510768286e9fac680511f976ea25c4117456b73
Dark_Eclipse <ilyapetrov847@gmail.com>
2024-01-15 02:46:20 +0300

    first-commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card-group">
+        <div className="small-card card"></div>
+        <div className="big-card card"></div>
+        <div className="small-card card"></div>
+        <div className="big-card card"></div>
+        <div className="small-card card"></div>
+        <div className="big-card card"></div>
+        <div className="small-card card"></div>
+        <div className="big-card card"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
